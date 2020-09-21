2e9865f9e64981233702d2b6ec164e6f129b080e
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-09-21 17:37:44 -0500

    initial JS up with ability to make new task lis upon button push

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-javascript
+var buttonEl = document.querySelector("#save-task");
+var tasksToDoEl = document.querySelector("#tasks-to-do");
+
+
+//buttonEl.addEventListener("click", function() {
+//    var taskItemEl = document.createElement("li");
+//    taskItemEl.className = "task-item";
+//    taskItemEl.textContent = "new task";
+//    tasksToDoEl.appendChild(taskItemEl);
+//})
+
+var createTaskHandeler = function() {
+    var taskItemEl = document.createElement("li");
+    taskItemEl.className = "task-item";
+    taskItemEl.textContent = "new task";
+    tasksToDoEl.appendChild(taskItemEl);
+
+}
+
+buttonEl.addEventListener("click", createTaskHandeler)
