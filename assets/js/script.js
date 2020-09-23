70a06696e92d97878ffc2bfa581642c9bab04535
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-09-23 13:12:24 -0500

    end 4.2 commit . has notes at botom for useful code learned.

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,12 +14,20 @@ var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-
     //package data as object
     var taskDataObj = {
         name: taskNameInput,
         type: taskTypeInput 
     }
+
+    //check for data
+    if (!taskNameInput || !taskTypeInput) {
+        alert("Please fill our all of the form.")
+        return false;
+    }
+    //reset screen (does not remove values for createTaskEL) (reset () only works on forms)
+    formEl.reset();
+
     //send it as an argumelt to createTaskEl
     creatTaskEl(taskDataObj);
 }
@@ -31,7 +39,6 @@ var creatTaskEl = function(taskDataObj) {
         //crate div to hold all task info (kid's info node) <div>
         var taskInfoEl = document.createElement("div");
 
-        
         //inside
             taskInfoEl.className = "task-info";
             taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
@@ -40,7 +47,6 @@ var creatTaskEl = function(taskDataObj) {
         
             //add listItemEl (parent node) to the taskToDoEl (grandparent node)
         tasksToDoEl.appendChild(listItemEl);
-
 }
 
 formEl.addEventListener("submit", taskFormHandler)
@@ -49,3 +55,9 @@ formEl.addEventListener("submit", taskFormHandler)
 //Note:
 // innerHTML means read as HTML - this alows for markips
 //textContent means reed as block of text - this alows for now inline markups all characters will render as a block of text
+// ! means NOT as in false value
+// <select> in html gives dropdown
+//submit = listener for a form so hiting enter works
+//event.PreventDefault(); stops page from reloading every time a form is submitted
+//all form data has value property whick we pull
+//reset() returns form to cleared mode 
