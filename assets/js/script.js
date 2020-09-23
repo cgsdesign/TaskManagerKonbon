8c387c5e7e01aee811b24753a103de133823eff0
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-09-23 15:02:04 -0500

    added buttons and unique id to task element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");//grandparent <ul>
-
+var taskIdCounter = 0;
 
 //buttonEl.addEventListener("click", function() {
 //    var taskItemEl = document.createElement("li");
@@ -36,18 +36,69 @@ var creatTaskEl = function(taskDataObj) {
         //create list item (parent node) <li>
         var listItemEl = document.createElement("li");
         listItemEl.className = "task-item";
+
+        // add task id to custom attribute
+        listItemEl.setAttribute("data-task-id", taskIdCounter)
+
         //crate div to hold all task info (kid's info node) <div>
         var taskInfoEl = document.createElement("div");
-
         //inside
             taskInfoEl.className = "task-info";
             taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
             //append/add the taskInfoEl node (kid's info node) to the listInfoEL (parent item node)
             listItemEl.appendChild(taskInfoEl);
-        
-            //add listItemEl (parent node) to the taskToDoEl (grandparent node)
+
+        //add in the create task action buttons
+        var taskActionsEl = createTaskAction(taskIdCounter);
+        listItemEl.appendChild(taskActionsEl)
+        //add listItemEl (parent node) to the taskToDoEl (grandparent node)
         tasksToDoEl.appendChild(listItemEl);
+
+        //increase task counter by 1 for next time
+        taskIdCounter++;
 }
+
+var createTaskAction = function(taskId) {
+    var actionContainerEl = document.createElement("div");
+    actionContainerEl.className = "task-actions";
+
+    //edit button
+    var editButtonEl = document.createElement("button");
+    editButtonEl.textContent = "Edit";
+    editButtonEl.className = "btn edit-btn";
+    editButtonEl.setAttribute("data-task-id", taskId);
+    // insert buton into div
+    actionContainerEl.appendChild(editButtonEl);
+
+    //delete button
+    var deleteButtonEl = document.createElement("button");
+    deleteButtonEl.textContent = "Delete";
+    deleteButtonEl.className = "btn delete-btn";
+    deleteButtonEl.setAttribute("data-task-id", taskId);
+    //insert delete buton into dev
+    actionContainerEl.appendChild(deleteButtonEl)
+
+    //select element where item is going (To Do, In Progress, Done)
+    var statusSelectEl = document.createElement("select");
+    var statusChoices= ["To Do", "In Progress", "Completed"];
+        for (var i = 0; i <statusChoices.length; i++) {
+            //crate option elements
+            var statusOptionEl = document.createElement("option");
+            statusOptionEl.textContent = statusChoices[i];
+            statusOptionEl.setAttribute("value", statusChoices[i]);
+            //insert selections
+            statusSelectEl.appendChild(statusOptionEl); 
+        }
+    statusSelectEl.className = "select-status";
+    statusSelectEl.setAttribute("name", "status-change");
+    statusSelectEl.setAttribute("data-task-id", taskId);
+    //insert select element into div
+    actionContainerEl.appendChild(statusSelectEl)
+
+    return actionContainerEl;
+
+}
+
 
 formEl.addEventListener("submit", taskFormHandler)
 
@@ -61,3 +112,5 @@ formEl.addEventListener("submit", taskFormHandler)
 //event.PreventDefault(); stops page from reloading every time a form is submitted
 //all form data has value property whick we pull
 //reset() returns form to cleared mode 
+// data-* is a custom data marker
+// .setAtribute alows you to ad an atribute tag  in the html
